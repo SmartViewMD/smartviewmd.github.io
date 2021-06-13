6e4c4c9a5e274fbc50e4efde65ae4731d6cd3808
monsieurverde <41913050+monsieurverde@users.noreply.github.com>
2021-06-13 18:32:43 -0400

    update download section

---
+++ precache-manifest.282c270e578ced101c58cd8a65459ddf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dac0353d6f5240fd5feabb8acb0c331",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e14bf37dd90dedf477b",
+    "url": "/static/css/2.2c846a47.chunk.css"
+  },
+  {
+    "revision": "1801003aaddbbde821a4",
+    "url": "/static/css/main.9ae72bb3.chunk.css"
+  },
+  {
+    "revision": "8e14bf37dd90dedf477b",
+    "url": "/static/js/2.730f2cea.chunk.js"
+  },
+  {
+    "revision": "1801003aaddbbde821a4",
+    "url": "/static/js/main.6a3e0372.chunk.js"
+  },
+  {
+    "revision": "017850d923ec93e60c64",
+    "url": "/static/js/runtime~main.7d96bdda.js"
+  },
+  {
+    "revision": "23c397f0b579edfc7912170d43921d62",
+    "url": "/static/media/bg-cta.23c397f0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4dd825d8d61f7c6bb830864d6fd22508.js"
+  "/precache-manifest.282c270e578ced101c58cd8a65459ddf.js"
 );
 
 self.addEventListener('message', (event) => {
